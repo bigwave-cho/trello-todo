026795deb4cb859c31dd1f26d1761e30342d2fb9
bigwave-cho <hooyaa93@gmail.com>
2023-02-25 16:42:31 +0900

    Recoil: set function of selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { hourSelector, minutesState } from './atoms';
 
 function App() {
+  /*
+  selector를 이용해서 state를 원하는대로
+  걸러 받을 수도 있고 역으로 state를 수정도 가능하다.
+
+  useRecoilState의 state
+  atom : atom value
+  selecor : get함수의 값
+  
+  두번째 요소
+  atom : atom 수정함수
+  selector : set 함수 실행
+  */
+  const [minutes, setMinutes] = useRecoilState(minutesState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+
   return (
     <>
-      <div className="App"></div>
+      <div className="App">
+        <input
+          value={minutes}
+          onChange={onMinutesChange}
+          type="number"
+          placeholder="Minutes"
+        />
+        <input
+          value={hours}
+          onChange={onHoursChange}
+          type="number"
+          placeholder="Hours"
+        />
+      </div>
     </>
   );
 }
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,1 +1,20 @@
 import { atom, selector } from 'recoil';
+
+export const minutesState = atom({
+  key: 'minutes',
+  default: 0,
+});
+
+export const hourSelector = selector<number>({
+  key: 'hours',
+  get: ({ get }) => {
+    const minutes = get(minutesState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    //minutesState를 10으로 만들겠다
+    // set(minutesState,10)
+    const minutes = +newValue * 60;
+    set(minutesState, minutes);
+  },
+});

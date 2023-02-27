9373f9d44a1f1781e359ba7dbfc12f7a4b611bdb
bigwave-cho <hooyaa93@gmail.com>
2023-02-27 14:03:07 +0900

    Beautiful-dnd : snapshot

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -9,6 +9,8 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -16,6 +18,18 @@ const Title = styled.h2`
   font-weight: 600;
   margin-bottom: 10px;
   font-size: 18px;
+`;
+
+interface IAreaProps {
+  isDraggingFromThis: boolean;
+  isDraggingOver: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver ? 'pink' : props.isDraggingFromThis ? 'red' : 'blue'};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
 `;
 
 interface IBoardProps {
@@ -28,14 +42,33 @@ function Board({ toDos, boardId }: IBoardProps) {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div ref={magic.innerRef} {...magic.droppableProps}>
-            {toDos.map((toDo, index) => (
-              <DragabbleCard key={toDo} index={index} toDo={toDo} />
-            ))}
-            {magic.placeholder}
-          </div>
-        )}
+        {(magic, snapshot) => {
+          /*
+          Droppablestate snapshot
+
+          isDraggingOver: boolean
+          현재 선택한 Draggable이 특정 Droppable위에 드래깅 되고 있는지 여부 확인
+
+          draggingOverWith: ?DraggableId
+          Droppable 위로 드래그하는 Draggable ID
+
+          draggingFromThisWith: ?DraggableId
+          현재 Droppable에서 벗어난 드래깅되고 있는 Draggable ID
+          */
+          return (
+            <Area
+              isDraggingOver={snapshot.isDraggingOver}
+              isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+              ref={magic.innerRef}
+              {...magic.droppableProps}
+            >
+              {toDos.map((toDo, index) => (
+                <DragabbleCard key={toDo} index={index} toDo={toDo} />
+              ))}
+              {magic.placeholder}
+            </Area>
+          );
+        }}
       </Droppable>
     </Wrapper>
   );
